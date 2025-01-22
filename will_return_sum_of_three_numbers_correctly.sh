00ec213533f5750f91fb7ba3ba6ea9e930fb6c3f
Urvi <urvisurti897@gmail.com>
2025-01-22 15:48:22 -0500

    Updated file with third value

--- will_return_sum_of_three_numbers_correctly.sh
+++ will_return_sum_of_three_numbers_correctly.sh
@@ -2,7 +2,7 @@
 
 # file: will_return_sum_of_two_numbers_correctly.sh
 
-RESULT=$(./sum_up_to_three_numbers.sh 13 15 10)
+RESULT=$(./sum_up_to_three_numbers.sh 13 15 6)
  
 if [[ $RESULT -eq 39 ]];
 then
